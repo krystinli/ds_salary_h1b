8938938aff76209bd74a8e63fbf9553ff72fd071
Krystin Li <krystin.li@capitalone.com>
2020-01-26 21:49:19 -0500

    complete data check

--- core.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-import re
-import pandas as pd
-import pickle
-import datetime as dt
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
